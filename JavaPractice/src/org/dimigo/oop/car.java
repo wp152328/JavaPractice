3f969b8014faa5e9a7af53bbeccdd6aec41c2257
jinprelude <jinprelude@218.38.137.28>
2017-03-23 11:38:42 +0900

    car

---
+++ JavaPractice/src/org/dimigo/oop/car.java
@@ -0,0 +1,41 @@
+package org.dimigo.oop;
+
+public class car {
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public String getCompany() {
+		return company;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public int  getPrice() {
+		return price;
+	}
+	
+	public void setCompany(String newCompany) {
+		company = newCompany;
+	}
+	public void setModel(String newModel) {
+		model = newModel;
+	}
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+	public void setMaxSpeed(int newMaxSpeed) {
+		maxSpeed = newMaxSpeed;
+	}
+	public void  setPrice(int newPrice) {
+		price = newPrice;
+	}
+}
